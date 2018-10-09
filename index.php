d1bf3c16280464227c2b887ae4bca48d840b7ca4
payal18 <payal18@iimshillong.ac.in>
2018-10-10 00:41:22 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,337 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <!-- ========== Title ========== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    <title> Golf Cup </title>
+    <!-- ========== Favicon Ico ========== -->
+    <!--<link rel="icon" href="fav.ico">-->
+    <!-- ========== STYLESHEETS ========== -->
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonts Icon CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/et-line.css" rel="stylesheet">
+    <link href="assets/css/ionicons.min.css" rel="stylesheet">
+    <!-- Carousel CSS -->
+    <link href="assets/css/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/css/owl.theme.default.min.css" rel="stylesheet">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <!--counter css-->
+    	<!-- Stylesheets -->	
+	<link rel="stylesheet" href="common-css/jquery.classycountdown.css" />
+		
+	<link href="01-comming-soon/css/styles.css" rel="stylesheet">
+	
+	<link href="01-comming-soon/css/responsive.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="loader">
+    <div class="loader-outter"></div>
+    <div class="loader-inner"></div>
+</div>
+<script> 
+        $(function(){
+            $("#include_header").load("header.html");
+        $("#include_footer").load("footer.html"); 
+        });
+</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!--header start here -->
+<div id="include_header"></div>
+<!--header end here-->
+
+<!--cover section slider -->
+<section id="home" class="home-cover">
+    <div id ="home_carousel" data-ride="carousel">
+    <div class="cover_slider owl-carousel owl-theme" >
+        <div class="cover_item" style="background: url('images/nb.jpg');">
+             <div class="slider_content">
+                <div class="slider-content-inner">
+                    <div class="container">
+                        <div class="slider-content-center">
+                            <h2 class="cover-title">
+                                Prepare yourself for 
+                            </h2>
+                            <strong class="cover-xl-text">Golf Cup</strong>
+                            <p class="cover-date">
+                                27th-28th October 2018  - IIM Shillong
+                            </p>
+                          <!--register 
+                            <a href="events.php" class=" btn btn-primary btn-rounded" >
+                                Register Now
+                            </a>
+							-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cover_item" style="background: url('images/n.jpg'); background-size: cover;">
+            <div class="slider_content">
+                <div class="slider-content-inner">
+                    <div class="container">
+                        <div class="slider-content-left">
+                            <h1 class="cover-title">
+                                Attract, Engage & Golf
+                            </h1>
+                            <!-- <strong class="cover-xl-text">Khlur-thma</strong>
+                            <p class="cover-date">
+                                26th-27th October 2018  - IIM Shillong.
+                            </p> -->
+                            <p><br></p>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cover_item"  style="background: url('images/na.jpg');">                      
+            <div class="slider_content">
+                    <!-- <video loop muted autoplay src="assets/video/khlurthma17.mp4"> </video>     -->
+                <div class="slider-content-inner">
+                    <div class="container">
+                        <div class="slider-content-center">
+                            <h2 class="cover-title">
+                                <!-- Prepare yourself for -->
+                            </h2>
+                            <!-- <strong class="cover-xl-text">Khlur-thma</strong> -->
+                            <p class="cover-date">
+                                <!-- 26th-27th October 2018  - IIM Shillong -->
+                            </p>
+                            <!-- <a href="events.php" class=" btn btn-primary btn-rounded" >
+                                    Register today
+                            </a> -->
+                            <div class="home-content__video">
+							
+							<video class="video-link" controls>
+  <source src="video/teaser_GolfCup.mp4" type="video/mp4">
+   <span class="video-icon"></span>
+   <span class="video-text">Play Video</span>
+
+ </video>
+                                   <!-- <a class="video-link" href="https://www.youtube.com/watch?v=1_nIJrVs4Qw" data-lity>
+                                        <span class="video-icon"></span>
+                                        <span class="video-text">Play Video</span>
+                                    </a> -->
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+   <!-- <div class="cover_nav">
+        <ol class="cover_dots">
+            <li data-target="#home_carousel" class="active" data-slide-to="0"><span>1</span></li>
+            <li  data-target="#home_carousel" data-slide-to="1"><span>2</span></li>
+            <li  data-target="#home_carousel" data-slide-to="2"><span>3</span></li>
+        </ol>
+    </div> -->
+</div>
+</section>
+
+<!--about the event -->
+<section class="pt100 pb100">
+    <div class="container">
+        <div class="section_title">
+            <h3 class="title">
+               IIM Shillong Golf Cup – Season 11
+            </h3>
+        </div>
+        <div class="row justify-content-center ">
+            <div class="col-12 col-md-4 ">
+			
+			<h5 class="heading_Text"> Golf Cup Season 11</h5>
+                <p class="boundary_Text" style="text-align:justify;">
+				
+                   The IIM Shillong Golf Cup offers a one of its kind experience and blends together a golf tournament, corporate discussions, knowledge workshops and networking dinners where corporate leaders, academicians, bureaucrats and other thought leaders come together to deliberate happenings across the business world.
+                 Every year the event hosts various dignitaries, corporate stalwarts, bureaucrats, policy makers, and students from across B-schools who come together at one of Asia’s largest natural, and wettest golf courses, the Gleneagles of the East, Shillong.
+				 IIM Shillong is the first and only B-School to host a golf tournament with a focussed aim of bringing together leaders from various business domains to share knowledge with budding managers on the golfing greens.
+				 </p>
+            </div>
+			 <div class="col-12 col-md-4 ">
+			 <h5 class="heading_Text"> Golf Cup Season 10</h5>
+                <p class="boundary_Text" style="text-align:justify">
+                    The event was organized in association with the Department of Meghalaya Tourism,the title sponsor of this event since 2009. Golf Cup Season 10 was officially teed off by Shri Sabyasachi Hajara, Former CMD Shipping Corporation of India and current member of the Board of Governors of IIM Shillong in presence of the Director of the Institute, Prof. Amitabha De on 11th November, 2017. It was successfully concluded on 12th November 2017 with a Valedictory Ceremony in the presence of the Chairman of IIM Shillong Shri Shishir Bajoria. The game was organized on a stroke play format with players ranked on the basis of finishing the 18-hole course in the least number of strokes played along with Stableford calculations. The efforts put in by the students were well appreciated by all the attendees and everyone looked up to the event as the right platform to engage and interact with the each other.  
+                </p>
+            </div>
+			 <div class="col-12 col-md-4 ">
+<!-- <h5 class="heading_Text"> About Golf Cup</h5>
+                <p class="boundary_Text">
+                   The Annual IIM Shillong Golf Cup may also be seen as a platform that provides participants of IIM Shillong with hands on experience in running operations towards successful conduct and completion.
+The tournament, first of its kind for any business school in India, is a unique and well-woven event that facilitates industry- academia participation and encourages professionals to “Look East”.
+The Golf Cup, sees keen involvement from state government ministers from across the North East, with the first day of the tournament dedicated to the local golfers and government officials. The second day sees the leading figures of the industry competing against each other and displaying exemplary sportsmanship. The event concludes with a gala ceremony concluding in the prize- distribution ceremony. The event is also embellished with a cultural show, with ethnic dance performances from across the north east, showcasing national multiculturalism and pluralism.
+ </p> -->
+ <article>
+                    <div class="fb-page" data-href="https://www.facebook.com/golfcup.iimshillong/" data-tabs="timeline" data-width="400" data-height="400"
+                        data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+                        <blockquote cite="https://www.facebook.com/golfcup.iimshillong/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/golfcup.iimshillong/">Golf Cup-Season 11</a></blockquote>
+                    </div>
+                    <!-- <footer>
+                                    <a href="#" class="button special">More</a>
+                                </footer> -->
+                </article>
+            </div>
+           
+        </div>
+
+     
+</section>
+<!--highlights about the event end -->
+
+<section class=" pb100">
+    <div class="container">
+        <div class="section_title">
+            <h3 class="title">
+               <u>Highlights</u>
+            </h3>
+        </div>
+        <div class="row justify-content-center ">
+            <div class="col-12 col-md-4 ">
+			
+			<h5 class="heading_Text"> International Participantion</h5>
+                <p class="boundary_Text" style="text-align:justify">
+				
+                   Golf Cup Season 11 is going international with Golfers expected from countries such as Singapore, Thailand, Myanmar and Bangladesh.
+                            Golf Cup is in line with the government's a "Act East" policy by being a platform to increase tourism in the North East by giving exposure to international Golfers from South and East Asia.
+             </p>
+            </div>
+			 <div class="col-12 col-md-4 ">
+			 <h5 class="heading_Text"> Student Category</h5>
+                <p class="boundary_Text" style="text-align:justify">
+                    IIM Shillong Amateur Golf Cup Season 11 will include a separate
+category for student golfers from across the country. This will
+serve as a platform for students from across the country to
+experience the rich heritage and natural beauty of Meghalaya.Through this initiative, Golf Cup plans to encourage and breed
+the Golfing sprit in Meghalaya.</p>
+            </div>
+			 <div class="col-12 col-md-4 ">
+			 <h5 class="heading_Text" > Expansion of Nurturing Minds</h5>
+                <p class="boundary_Text" style="text-align:justify">
+                    The initiative in memory of Dr. A.P.J. Abdul Kalam will be
+expanded by the students of IIM Shillong to include
+dance classes and golf sessions for the children from the 
+underprivileged sections of the society. This initiative will
+be carried out throughout the year on every Saturday. The school students will also be given a chance to play in the golf course.</p>
+            </div>
+           
+        </div>
+
+     
+</section>
+
+<!--event countdown -->
+<section class="bg-img pt70 pb70" style="background-image: url('assets/img/Archieves/a.png');">
+    <div class="overlay_dark"></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10">
+                <h4 class="mb30 text-center color-light">Counter until the big event</h4>
+                <div class="countdown-base"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!--brands section -->
+<section id="sponsors" class="bg-gray pt100 pb100">
+    <div class="container">
+        <div class="section_title mb50">
+            <h3 class="title">
+                previous associations
+            </h3>
+        </div>
+        
+        <div class="brand_carousel owl-carousel">
+            <div class="brand_item text-center">
+                <img src="assets/img/brands/mt.png" alt="brand">
+            </div>
+            <div class="brand_item text-center">
+                <img src="assets/img/brands/gs.png" alt="brand" vspace="10">
+            </div>
+
+             <div class="justify-content-center align-items-center">
+                <img src="assets/img/brands/sbi.png" align= "middle" alt="brand">
+            </div>
+			 <div class="brand_item text-center">
+                <img src="assets/img/brands/io.png" alt="brand">
+            </div>
+            <div class="justify-content-center align-items-center">
+                <img src="assets/img/brands/hsbc.png" alt="brand" vspace="30">
+            </div>
+
+             <div class="brand_item text-center">
+                <img src="assets/img/brands/ey.png" alt="brand">
+            </div>
+			 <div class="brand_item text-center">
+                <img src="assets/img/brands/ub.png" alt="brand">
+            </div>
+            <div class="justify-content-center align-items-center">
+                <img src="assets/img/brands/ubi.png" alt="brand" vspace="30">
+            </div>
+			<div class="brand_item text-center">
+                <img src="assets/img/brands/uco.png" alt="brand">
+            </div>
+			<div class="justify-content-center align-items-center" vspace="50">
+                <img src="assets/img/brands/vm.png" alt="brand">
+            </div>
+			 </div>
+        
+			<div class= "pt50  justify-content-center " style="margin-left:500px">
+			 <a href="sponsor.php" class=" btn-Register  btn-rounded color-light" >
+                               Click to associate
+                            </a>
+        </div>
+		
+   
+    </div>
+</section>
+<!--brands section end-->
+<!--footer start -->
+ 
+<div id="include_footer"></div>
+<!--footer end -->
+
+<!-- jquery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- bootstrap -->
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<!--slick carousel -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!--parallax -->
+<script src="assets/js/parallax.min.js"></script>
+<!--Counter up -->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!--Counter down -->
+<script src="assets/js/jquery.countdown.min.js"></script>
+<!-- WOW JS -->
+<script src="assets/js/wow.min.js"></script>
+<!-- Custom js -->
+<script src="assets/js/main.js"></script>
+<!-- counter js-->
+<script src="common-js/jquery.classycountdown.js"></script>
+<script src="common-js/jquery.knob.js"></script>
+<script src="common-js/jquery.throttle.js"></script>
+<script src="common-js/scripts.js"></script>
+</body>
+</html>
---
+++ sponsor.php
@@ -0,0 +1,189 @@
+<?php include('dbconfig.php'); ?>
+    <?php
+    $nameErr =$emailErr=$contactErr =$instituteErr = "";
+    $name=$email=$contact=$institute=$event="";
+    $formError="";
+    $query="";
+    
+    // if($_GET['event']=="justbidit") {
+    //     $event="Just Bid It";
+    // } else 
+    
+    if(isset($_POST['register']))
+    {
+        $name =$_POST['name'];
+        $email = $_POST['email'];
+        $contact = $_POST['contact'];
+        $company = $_POST['company'];
+       
+        $required="Required field";
+        $sql = "INSERT INTO sponsors (name1,email1,contact1,company) 
+        VALUES ('$name','$email','$contact','$company')";
+
+        #echo $sql;
+        if(!$name) {
+            $nameErr=$required;
+        } else{
+            if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+                $nameErr = "Only letters and white space allowed"; 
+            }
+        }
+        if(!$email) {
+            $emailErr=$required;
+        }else {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email";
+            }
+        }
+        if(!$contact) {
+            $contactErr=$required;
+        } else {
+            if(!preg_match("/^(\+\d{1,3}[- ]?)?\d{10}$/", $contact)) {
+                $contactErr = "Enter valid contact number";
+            }
+        }
+        if(!$company) {
+            $instituteErr=$required;
+        }
+        
+        if(!$emailErr && !$nameErr && !$contactErr && !$instituteErr) {
+            #echo $sql;
+            $result = mysqli_query($conn,$sql);
+            if(!$result) {
+                #echo "did not insert". mysql_error();
+            }
+            $formError="You have registered successfully";
+        } else {
+            $formError= "Fill all the details correctly";
+        }
+    }
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    <!-- ========== Title ========== -->
+    <title> Khlurthma</title>
+    <!-- ========== Favicon Ico ========== -->
+    <!--<link rel="icon" href="fav.ico">-->
+    <!-- ========== STYLESHEETS ========== -->
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonts Icon CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/et-line.css" rel="stylesheet">
+    <link href="assets/css/ionicons.min.css" rel="stylesheet">
+    <!-- Carousel CSS -->
+    <link href="assets/css/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/css/owl.theme.default.min.css" rel="stylesheet">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+<div class="loader">
+    <div class="loader-outter"></div>
+    <div class="loader-inner"></div>
+</div>
+
+<script> 
+        $(function(){
+            $("#include_header").load("header.html");
+            $("#include_footer").load("footer.html"); 
+        });
+</script>
+<!--header start here -->
+<div id=include_header></div>
+<!--header end here-->
+
+<!--page title section-->
+<section class="inner_cover parallax-window" data-parallax="scroll" data-image-src="assets/img/bg/bg-img.png">
+    <div class="overlay_dark"></div>
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12">
+                <div class="inner_cover_content">
+                    <h3>
+                    Sponsorship Details
+                    </h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="breadcrumbs">
+            <ul>
+                <li><a href="index.php">Home</a>  |   </li>
+                <li><span>Sponsorship</span></li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--page title section end-->
+
+
+<!--events section -->
+<section class="pt100 pb100">
+    <div class="container">
+
+        <div class="col-md-6 col-12">
+            <div class="contact_form">
+                <span class="error" style="color:#f50136"><strong><?php echo $formError;?></strong></span>
+                <form action="<?php $event= $query;?>" method="POST">
+                <div class="form-group">
+                <h4>Details</h4>
+                <span class="error" style="color:#f50136"><?php echo $nameErr;?></span>
+                    <input type="text" name="name"  class="form-control" placeholder="Name">
+                </div>
+                <div class="form-group">
+                <span class="error" style="color:#f50136"><?php echo $emailErr;?></span>
+                    <input type="text" name="email" class="form-control" placeholder="Email">
+                </div>
+                <div class="form-group">
+                <span class="error" style="color:#f50136"><?php echo $contactErr;?></span>
+                    <input type="text" name="contact" class="form-control" placeholder="Contact">
+                </div>
+                <div class="form-group text-right">
+                    <input type="submit" class="btn btn-rounded btn-primary" name="register" value="Register">
+                </div>
+            </form>
+            </div>
+        </div> 
+    </div>
+</section>
+
+<div id="include_footer">
+    
+<div class="copyright_footer">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-12">
+                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | <a href="https://colorlib.com" target="_blank">Colorlib</a>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- jquery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- bootstrap -->
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<!--slick carousel -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!--parallax -->
+<script src="assets/js/parallax.min.js"></script>
+<!--Counter up -->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!--Counter down -->
+<script src="assets/js/jquery.countdown.min.js"></script>
+<!-- WOW JS -->
+<script src="assets/js/wow.min.js"></script>
+<!-- Custom js -->
+<script src="assets/js/main.js"></script>
+</body>
+</html>
